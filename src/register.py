2649e1302679698eac331f17d40605ff5f5a24f8
Henry Chen <tahafut@gmail.com>
2025-08-20 15:23:16 +0100

    remove Register class

--- src/register.py
+++
@@ -1,9 +0,0 @@
-class Register:
-    def __init__(self):
-        self._contents = None
-
-    def get_contents(self):
-        return self._contents
-
-    def set_contents(self, contents):
-        self._contents = contents
--- test/test_register.py
+++
@@ -1,7 +0,0 @@
-from src.register import Register
-
-
-def test_register():
-    register = Register()
-    register.set_contents("hello")
-    assert register.get_contents() == "hello"
